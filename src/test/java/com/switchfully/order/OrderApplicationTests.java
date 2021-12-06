c34ed7dc93fcd1bb100b4aada1f4ff3a0c1cba39
RubenNeven <rubenneven@gmail.com>
2021-12-06 14:11:38 +0100

    Setup swagger

---
+++ src/test/java/com/switchfully/order/OrderApplicationTests.java
@@ -0,0 +1,11 @@
+package com.switchfully.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+
+
+}
