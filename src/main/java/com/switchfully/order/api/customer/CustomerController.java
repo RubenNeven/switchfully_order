eb36bfbb6c04187e0c2838dd536d0faaa4f96145
RubenNeven <rubenneven@gmail.com>
2021-12-06 15:38:41 +0100

    Story 1: Create a customer account (adding logging)

--- src/main/java/com/switchfully/order/api/customer/CustomerController.java
+++ src/main/java/com/switchfully/order/api/customer/CustomerController.java
@@ -3,16 +3,20 @@ package com.switchfully.order.api.customer;
 import com.switchfully.order.domain.customer.Customer;
 import com.switchfully.order.domain.customer.CustomerDto;
 import com.switchfully.order.service.customer.CustomerServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
 
+    Logger logger = LoggerFactory.getLogger(CustomerController.class);
     private final CustomerServiceImpl customerServiceImpl;
 
     @Autowired
@@ -23,12 +27,14 @@ public class CustomerController {
     @GetMapping(produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public List<CustomerDto> getAllCustomers() {
+        logger.info("Get all customers!");
         return customerServiceImpl.getAllCustomers();
     }
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public Customer createCustomer(@RequestBody CustomerDto customerDto){
+        logger.info("Create customer called!");
         return customerServiceImpl.createCustomer(customerDto);
     }
 }
